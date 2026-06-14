9de24e47f4077665a4e15f809aca5f8ac0335772
SAKSHAM SINGH <singhsaksham7376@gmail.com>
2026-06-14 22:25:04 +0530

    changes to be committed:

---
+++ config.js
@@ -0,0 +1,3 @@
+// Add your YouTube Data API v3 key here to enable direct video redirects.
+// If you leave the placeholder, the app will fallback to YouTube search results.
+const YT_API_KEY = 'YOUR_API_KEY_HERE';
